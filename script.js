de5f9dd758ac5c645e6633889b36105ecd7913ff
Patrick Jongbloed <patrick@jongbloeds.com>
2021-04-02 20:36:47 +0000

    updated js file

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+var date = new Date();
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    interest = principle * years * rate / 100;
+
+    targetDate = date.addDays(years*365);
+    targetYear = targetDate.getFullYear();
+}
+
+function getRangeValue()
+{
+    rateValue = document.getElementById("rate").value;
+    document.getElementById("displayRate").innerHTML = rateValue;
 }
         
