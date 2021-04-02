356f3e8c65a4bfa8e7757cba074b710fcda6a11d
Patrick Jongbloed <patrick@jongbloeds.com>
2021-04-02 21:14:05 +0000

    adding compute funcionality

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ function compute()
 
     targetDate = date.addDays(years*365);
     targetYear = targetDate.getFullYear();
+
+    result = ("If you deposit " + principal + ","<br>
+              "at an interest rate of " + rate + "."<br>
+              "You will receive an amount of " + interest + ","<br>
+              "in the year " + targetYear);
+    document.getElementById("result").innerHTML = result;
 }
 
 function getRangeValue()
